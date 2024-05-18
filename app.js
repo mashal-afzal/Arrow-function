a4ee935fcc895a5fee3741c4ae193c99e0872d00
Mashal Afzal <mashal.afzal2996@gmail.com>
2024-05-18 19:05:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+"use strict";
+let fullName = (name1, name2, name3) => name1 + " " + name2 + " " + name3;
+let result = fullName("Mashal", "Afzal", "Ali");
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,4 @@
+let fullName= (name1:string, name2:string, name3:string) => name1  + " " + name2  + " "  + name3
+let result=fullName("Mashal",   "Afzal" ,  "Ali")
+console.log(result)
+
